6d71f6c367135a3104d81cd44c7af295c4059d55
Paul Rohozhyn <dev.rohozyn@gmail.com>
2025-01-12 19:01:29 +0100

    task3 task4

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,19 @@
 console.log('************************ task-3 *******************************');
 
-// function getElementWidth(content, padding, border) {
-//     return Math.round(Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2));
-// }
+function checkForSpam(message) {
+    const words = ['spam', 'sale'];
+    for (let i = 0; i <= words.length-1; i++) { 
+        if (message.toLowerCase().includes(words[i])) {
+            return true;
+        }
+    }
+    return false;
+}
 
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,33 @@
 console.log('************************ task-4 *******************************');
 
-// function getElementWidth(content, padding, border) {
-//     return Math.round(Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2));
-// }
+function getShippingCost(country) {
 
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+    let tarrif = 0;
+    switch (country)
+    {
+        case 'China':
+            tarrif = 100;
+            break;
+        case 'Chile':
+            tarrif = 250;
+            break;
+        case 'Australia':
+            tarrif = 170;
+            break;
+        case 'Jamaica':
+            tarrif = 120;
+            break;
+        default:
+            tarrif = 0;
+            break;
+    }
+
+    return tarrif ? `Shipping to ${country} will cost ${tarrif} credits` : "Sorry, there is no delivery to your country";
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
